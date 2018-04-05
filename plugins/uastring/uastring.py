f02fee55dd28726443a39734c21471654db6e028
Tony <anlee2@vt.edu>
2018-04-05 19:12:23 +0800

    Added uastring plugin
    Added uastring plugin

---
+++ plugins/uastring/uastring.py
@@ -0,0 +1,24 @@
+# !uastring parses a user agent string and performs a lookup to determine OS and browser.
+
+import os, requests, json, urllib.parse
+from errbot import BotPlugin, botcmd, arg_botcmd
+
+class uastring(BotPlugin):
+
+    @arg_botcmd('uastring', type=str)  # flags a command
+    def uastring(self, msg, uastring=None):
+
+        raw_param = { 'uas' : uastring}
+        encoded_param=urllib.parse.urlencode(raw_param)
+        urlip = "http://www.useragentstring.com/?getJSON=all&" + encoded_param
+
+        try:
+            response = requests.get(urlip)
+        except:
+            return("Could not retrieve page")
+            exit()
+
+        #read html code
+        data = response.json()
+        return(json.dumps(data, sort_keys=True, indent=4))
+
