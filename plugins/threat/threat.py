b4edb8a6fe7f48f28413cf84b7359669cf58675c
Tony <anlee2@vt.edu>
2018-12-31 11:38:01 -0500

    Add files via upload
    Updating with new easier to read output format

---
+++ plugins/threat/threat.py
@@ -0,0 +1,56 @@
+# !threat is used to query a Google spreadsheet containing information on Mitre Attack Matrix Mapping
+
+import os, requests, re, csv, io, sys
+from errbot import BotPlugin, botcmd, arg_botcmd
+
+class threat(BotPlugin):
+
+    @arg_botcmd('query', type=str) # flags a command
+    def threat(self, msg, query=None):
+
+        # Create headers for HTTP request
+        headers={}
+        headers["User-Agent"]= "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:22.0) Gecko/20100101 Firefox/22.0"
+        headers["DNT"]= "1"
+        headers["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+        headers["Accept-Encoding"] = "deflate"
+        headers["Accept-Language"]= "en-US,en;q=0.5"
+
+        # Construct the URL to fetch the spreadsheet
+        file_id="1ljXt_ct2J7TuQ45KtvGppHwZUVF7lNxiaAKII6frhOs"
+        url = "https://docs.google.com/spreadsheets/d/{0}/pub?gid=290745198&output=csv".format(file_id)
+
+        try:
+            r = requests.get(url, headers=headers)
+        except:
+            return("Error:  Could not retrieve time")
+            exit()
+
+        #read html code
+        sio = io.StringIO( r.text, newline=None)
+        reader = csv.DictReader(sio, dialect=csv.excel)
+        
+        # Build answer to return it
+        answer=""
+        for row in reader:
+            m = re.search(query, str(row), re.IGNORECASE)
+            if m:
+                answer = answer + "\r\n##Category: " + row["Category"] + "##"
+                answer = answer + "\r\nTechnique: " + row["Technique"]
+                answer = answer + "\r\nName: " + row["Name"]
+                answer = answer + "\r\nType: " + row["Type"]
+                answer = answer + "\r\nUsage: " + row["Usage"]
+                answer = answer + "\r\nData Sources: " + row["Data Sources"]
+                answer = answer + "\r\nPlatform: " + row["Platform"]
+                answer = answer + "\r\nID: " + row["ID"]
+                answer = answer + "\r\nMITRE ID: " + row["MITRE ID"]
+                answer = answer + "\r\nCAPEC ID: " + row["CAPEC ID"]
+                answer = answer + "\r\nTechnical Description: " + row["Technical Description"]
+
+        if not answer:
+            answer = "Did not find anything.  Please refine your search."
+
+        return(answer)
+
+
+
