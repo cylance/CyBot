cc80165a6102fdc382fddfce904fcf24b8d7a689
Tony <anlee2@vt.edu>
2019-09-01 18:18:08 -0400

    Update cve.py
    Cleaned up the output to make it more human readable

--- plugins/cve/cve.py
+++ plugins/cve/cve.py
@@ -15,20 +15,31 @@ class cve(BotPlugin):
         # Assign arguments to variables
         arg=args[0]
         if re.match(r'^[\d]{4}\-[\d]{4}$', arg):
-          urlip = "http://cve.circl.lu/api/cve/CVE-" + arg
+          url = "http://cve.circl.lu/api/cve/CVE-" + arg
           try:
-              response = requests.get(urlip)
+              response = requests.get(url)
           except:
               return("Could not retrieve page")
               exit()
         elif re.match(r'^[\d]+$', arg):
-          urlip = "http://cve.circl.lu/api/last/" + arg
+          url = "http://cve.circl.lu/api/last/" + arg
           try:
-              response = requests.get(urlip)
+              response = requests.get(url)
           except:
               return("Could not retrieve page")
               exit()
         else:
           return("Error: Input must be an integer or a CVE number in the format: ####-####.")
 
-        return(response.text)
+        data_list = response.json()
+        answer = ""
+        #iterate over a list of dict entries
+        for json_entry in data_list:
+            answer+="##ID:  " + str(json_entry["id"]) + "\r\n"
+            answer+="Summary:  " + str(json_entry["summary"]) + "\r\n"
+            answer+="Published:  " + str(json_entry["Published"]) + "\r\n"
+            answer+="Last Modifiedd:  " + str(json_entry["last-modified"]) + "\r\n"
+            answer+="CVSS:  " + str(json_entry["cvss"]) + "\r\n"
+            answer+="References:  " + str(json_entry["references"]) + "\r\n\r\n"
+
+        return(answer)
