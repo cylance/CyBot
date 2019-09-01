2b7bb6d28779ca6ec6e912b9c9a074e3a4e0dfef
Tony <anlee2@vt.edu>
2019-09-01 16:40:44 -0400

    Update geoip.py
    Processes defanged URLs and the output is cleaned up to make it more human readable.

--- plugins/geoip/geoip.py
+++ plugins/geoip/geoip.py
@@ -6,17 +6,34 @@ from errbot import BotPlugin, botcmd, arg_botcmd
 class geoip(BotPlugin):
 
     @arg_botcmd('query', type=str)  # flags a command
-    def geoip(self, msg, query=None): 
-        url = query
+    def geoip(self, msg, query=None):
+        #Process defanged FQDN
+        query = re.sub('[\[()\]]', '', query)
         headers = {
             "Accept-Encoding": "gzip, deflate",
             "User-Agent" : "You all rock!"
             }
-        response = requests.get('http://ip-api.com/json/' + url,
+        response = requests.get('http://ip-api.com/json/' + query,
             headers=headers)
 
-        str_resp = response.text
-        str_resp = re.sub('{', '', str_resp)
-        str_resp = re.sub('}', '', str_resp)
-        str_resp = re.sub(',', '\r\n', str_resp)
-        return(str_resp)
+        json_resp = response.json()
+#        print(json_resp)
+        if json_resp["status"] == "success":
+            answer = "Status: " + json_resp["status"] + "\r\n"
+            answer += "Query: " + str(json_resp["query"]) + "\r\n"
+            answer += "Org: " + str(json_resp["org"]) + "\r\n"
+            answer += "ISP: " + str(json_resp["isp"]) + "\r\n"
+            answer += "AS: " + str(json_resp["as"]) + "\r\n"
+            answer += "Timezone: " + json_resp["timezone"] + "\r\n"
+            answer += "Country: " + json_resp["country"] + "\r\n"
+            answer += "Country Code: " + json_resp["countryCode"] + "\r\n"
+            answer += "Region Name: " + json_resp["regionName"] + "\r\n"
+            answer += "City: " + json_resp["city"] + "\r\n"
+            answer += "Zip: " + json_resp["zip"] + "\r\n"
+            answer += "Lat, Long: (" + str(json_resp["lat"]) + ", " + str(json_resp["lon"]) + ")\r\n"
+            answer += "Best guess on a map: https://www.latlong.net/c/?lat=" + str(json_resp["lat"]) + "&long=" + str(json_resp["lon"]) + "\r\n"
+        else:
+            answer = "Status: " + json_resp["status"] + "\r\n"
+            answer += "Message: " + json_resp["message"] + "\r\n"
+
+        return(answer)
