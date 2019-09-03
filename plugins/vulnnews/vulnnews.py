70c9a50b9446c1b15b78558cfed5b03322ca0255
Tony <anlee2@vt.edu>
2019-09-03 16:55:43 -0400

    Update vulnnews.py
    Now accepts a parameter for number of articles to return.  Also now defaults to 10 articles.

--- plugins/vulnnews/vulnnews.py
+++ plugins/vulnnews/vulnnews.py
@@ -1,14 +1,29 @@
-# !vulnnews is used for querying Google News for CVE Vulnerability
+# !vulnnews is used for qurying Google News for CVE Vulnerability
 
-import requests
+import requests, re
 import xml.etree.ElementTree as ET
 from errbot import BotPlugin, botcmd, arg_botcmd
 
 class vulnnews(BotPlugin):
 
-    @botcmd # flags a command
+    @botcmd(split_args_with=None, hidden=True)
     def vulnnews(self, msg, args):
+
+        #Define default number of articles
+        limit=10
         urlip = "https://news.google.com/news/rss/search/section/q/CVE%20vulnerability/CVE%20vulnerability?hl=en&gl=US&ned=us"
+
+        print(len(args))
+        if len(args)!=1 and len(args)!=0:
+            return("Incorrect number of parameters.  Can either be 0 or 1 arguments.")
+            exit()
+
+        if len(args)==1:
+            if args[0].isdigit():
+                limit=int(args[0])
+            else:
+                return("Only acceptable argument is a number to specify the article count.")
+                exit()
 
         try:
             website = requests.get(urlip)
@@ -29,10 +44,10 @@ class vulnnews(BotPlugin):
                 answer += "\r\n"
                 answer += link
                 count += 1
+                if count>limit:
+                    break
 
             return(answer)
-
         except:
             return("Could not retrieve page")
             exit()
-
