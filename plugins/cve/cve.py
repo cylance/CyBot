0023394d87a2be7cf7626728ded12bab1742590d
Tony <anlee2@vt.edu>
2019-09-03 14:52:27 -0400

    Update cve.py
    CVE length Bug reported by siffo.  Fixed the bug and caught another bug in the process:  Specifying a specific CVE returns a json entry.  Specifying the last n CVEs returns a list of json entries.  Also added error checking for CVEs that do not exist in the CVE database.
    
    https://cve.mitre.org/cve/identifiers/syntaxchange.html#new

--- plugins/cve/cve.py
+++ plugins/cve/cve.py
@@ -9,37 +9,52 @@ class cve(BotPlugin):
     def cve(self, mess, args):
 
         if len(args)!=1:
-            return("Incorrect number of parameters.  Must either specifcy the CVE number ####-#### or a digit # for the most recent N CVE's")
+            return("Incorrect number of parameters.  Must either specifcy the CVE number YYYY-####[###] or a digit # for the most recent N CVE's")
             exit()
 
         # Assign arguments to variables
-        arg=args[0]
-        if re.match(r'^[\d]{4}\-[\d]{4}$', arg):
-          url = "http://cve.circl.lu/api/cve/CVE-" + arg
-          try:
-              response = requests.get(url)
-          except:
-              return("Could not retrieve page")
-              exit()
-        elif re.match(r'^[\d]+$', arg):
-          url = "http://cve.circl.lu/api/last/" + arg
-          try:
-              response = requests.get(url)
-          except:
-              return("Could not retrieve page")
-              exit()
-        else:
-          return("Error: Input must be an integer or a CVE number in the format: ####-####.")
-
-        data_list = response.json()
         answer = ""
-        #iterate over a list of dict entries
-        for json_entry in data_list:
+        arg=args[0]
+        if re.match(r'^[\d]{4}\-[\d]{4,7}$', arg): # Specific CVE - response is json
+            url = "http://cve.circl.lu/api/cve/CVE-" + arg
+            try:
+                response = requests.get(url)
+            except:
+                return("Could not retrieve page")
+                exit()
+
+            json_entry = response.json()
+            if json_entry is None:  #Check to make sure the CVE was valid
+                return("CVE Not Found.  Check the ID and try again.")
+
+            print(json_entry)
             answer+="##ID:  " + str(json_entry["id"]) + "\r\n"
             answer+="Summary:  " + str(json_entry["summary"]) + "\r\n"
             answer+="Published:  " + str(json_entry["Published"]) + "\r\n"
             answer+="Last Modifiedd:  " + str(json_entry["last-modified"]) + "\r\n"
             answer+="CVSS:  " + str(json_entry["cvss"]) + "\r\n"
             answer+="References:  " + str(json_entry["references"]) + "\r\n\r\n"
+
+        elif re.match(r'^[\d]+$', arg): # Last n CVEs - response is list of json
+            url = "http://cve.circl.lu/api/last/" + arg
+            try:
+                response = requests.get(url)
+                print(response)
+            except:
+                return("Could not retrieve page")
+                exit()
+
+            data_list = response.json()
+            #iterate over a list of dict entries
+            for json_entry in data_list:
+                answer+="##ID:  " + str(json_entry["id"]) + "\r\n"
+                answer+="Summary:  " + str(json_entry["summary"]) + "\r\n"
+                answer+="Published:  " + str(json_entry["Published"]) + "\r\n"
+                answer+="Last Modifiedd:  " + str(json_entry["last-modified"]) + "\r\n"
+                answer+="CVSS:  " + str(json_entry["cvss"]) + "\r\n"
+                answer+="References:  " + str(json_entry["references"]) + "\r\n\r\n"
+
+        else:
+            return("Error: Input must be an integer or a CVE number in the format: YYYY-####[###].")
 
         return(answer)
